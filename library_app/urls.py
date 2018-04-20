365209d71a2696033c6996d71a89df97c9d85bfc
Narcisa96 <narcisarusu96@yahoo.com>
2018-04-20 04:11:41 +0300

    Login/Logout

--- library_app/urls.py
+++ library_app/urls.py
@@ -4,7 +4,15 @@ from . import views
 urlpatterns = [
     url(r'^$',
         views.BookListView.as_view(), name='home'),
+
     url(r'^book/(?P<pk>[0-9]+)/$',
-        views.DetailsListView.as_view(),
-        name='book_details'),
+        views.DetailsListView.as_view(), name='book_details'),
+
+    url(r'^login',
+        views.login_view,
+        name="login"),
+
+    url(r'^logout/$',
+        views.logout_view,
+        name="logout"),
     ]
--- library_app/views.py
+++ library_app/views.py
@@ -1,11 +1,38 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.views.generic import (TemplateView, ListView, CreateView, DetailView, DeleteView)
 from models import Book
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
+from forms import LoginForm
 
+
+def login_view(request):
+    context = {}
+    if request.method == 'GET':
+        form = LoginForm
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user:
+                login(request=request,
+                      user=user)
+                return redirect('home')
+            else:
+                context['error_message'] = 'Wrong username or password!'
+    context['form'] = form
+    return render(request, 'login.html', context)
+
+
+def logout_view(request):
+    if request.method == 'GET':
+        logout(request)
+        return redirect('home')
 
 class BookListView(ListView):
     template_name = 'home.html'
