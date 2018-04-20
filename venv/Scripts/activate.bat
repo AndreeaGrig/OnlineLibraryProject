242d8a06f7a173dcb5f61f9ccbdc1fb3da2dac63
andreea <andreea_grig@ymail.com>
2018-04-20 11:58:25 +0300

    adaugare css

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\User\Documents\GitHub\OnlineLibraryProject\venv"
+set "VIRTUAL_ENV=C:\Users\andre\PycharmProjects\OnlineLibraryProject\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
