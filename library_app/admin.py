a2796836f94aa82f5c887ff2c4180aa81485b5e5
andreea <andreea_grig@ymail.com>
2018-04-20 14:26:24 +0300

    adaugare css

--- library_app/admin.py
+++ library_app/admin.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
-from library_app.models import Book
+from library_app.models import Book, Review
 
 # Register your models here.
 admin.site.register(Book)
+admin.site.register(Review)
 
--- library_app/views.py
+++ library_app/views.py
@@ -47,3 +47,4 @@ class DetailsListView(DetailView):
 
 
 
+
