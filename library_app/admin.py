919dd9cea25c2e63374795264b2b185dc7a0797e
Narcisa96 <narcisarusu96@yahoo.com>
2018-05-07 18:43:56 +0300

    Reset Password

--- library_app/admin.py
+++ library_app/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
+
 from library_app.models import Book, Review, Purchase
 
 # Register your models here.
