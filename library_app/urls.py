cfb1d17691f5213aa3c49343725b6fa3d854f99d
Narcisa96 <narcisarusu96@yahoo.com>
2018-05-07 19:45:20 +0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 66f0c07a29b0012cbb6c971f6da833cd8e94a0e9.

--- library_app/urls.py
+++ library_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
         views.BookListView.as_view(), name='home'),
 
     url(r'^book/(?P<pk>[0-9]+)/$',
-        views.RecommendationListView.as_view(), name='book_details'),
+        views.DetailsListView.as_view(), name='book_details'),
 
     url(r'^login',
         views.login_view,
