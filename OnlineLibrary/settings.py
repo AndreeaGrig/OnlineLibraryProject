5f1079afc6cc915ac2a911f38d0e14f95a7301a1
Narcisa96 <narcisarusu96@yahoo.com>
2018-05-07 19:05:47 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	db.sqlite3

--- OnlineLibrary/settings.py
+++ OnlineLibrary/settings.py
@@ -132,10 +132,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'iussof31@gmail.com'
 EMAIL_HOST_PASSWORD = 'laptopacer'
 EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 AUTHENTICATION_BACKENDS = [
-    'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
-    'social_core.backends.google.GoogleOpenId',  # for Google authentication
     'social_core.backends.google.GoogleOAuth2',  # for Google authentication
 
     'django.contrib.auth.backends.ModelBackend',
